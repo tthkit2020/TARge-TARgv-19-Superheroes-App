c815ab1a60669b7942b5559028a5e77ff5821cd7
Julia Voronetskaja <julia.voronetskaja@tthk.ee>
2020-09-09 10:51:10 +0300

    Add Supervillains

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 let superheroes = require('superheroes');
-console.log(superheroes.random());
+let supervillains = require('supervillains');
+//console.log(superheroes.random());
 
-superheroes.all.forEach(hero => {
-    console.log(hero);
+supervillains.all.forEach(supervillain => {
+    console.log(supervillain);
 });
 
+console.log(supervillains.random());
+
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
+
+
+/*superheroes.all.forEach(hero => {
+    console.log(hero);
+});*/
+
 //console.log(superheroes.all);
+
+/* TASK1
+1. Get the names of all supervillains
+2. Get a random supervillain name
+3.* Make a random superhero fight a random supervillain 
+in the console.log()
+
+*/
